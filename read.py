23997f3b34d6fd1a2fc67c1ed8669a227b147deb
Hsieh Yi Ting <hsiehyiting@Hsiehde-MacBook-Pro.local>
2020-11-08 14:57:04 +0800

    list comprehension

--- read.py
+++ read.py
@@ -23,10 +23,12 @@ for d in data:
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 
-good = []
-for d in data:
-	if 'good' in d:
-		good.append(d)
+good = [d for d in data if 'good' in d ]
 
 print('一共有', len(good), '筆留言提到good')
 print(good[0])
+
+
+bad = ['bad' in d for d in data]
+print(bad)
+
